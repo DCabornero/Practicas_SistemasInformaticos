498cc3a47b214930edd21721b55c21d69fc629b6
sergamar <sergamar23@hotmail.es>
2019-10-24 19:54:23 +0200

    Ultima semana

--- P2/app/routes.py
+++ P2/app/routes.py
@@ -8,6 +8,7 @@ import os
 import sys
 import hashlib
 import random
+from datetime import date
 
 @app.route('/')
 @app.route('/index')
@@ -95,7 +96,7 @@ def registro():
         os.mknod(os.path.join(app.root_path, 'usuarios/'+request.form['email']+'/datos.json'))
         os.mknod(os.path.join(app.root_path, 'usuarios/'+request.form['email']+'/historial.json'))
         historial = {
-            "peliculas": []
+            "pedidos": []
         }
         datos = {
             "nombre": request.form['nombre'],
@@ -106,11 +107,11 @@ def registro():
             "contrasena": hashlib.md5(request.form['contrasena'].encode()).hexdigest(),
             "saldo": random.randint(0,100)
         }
-        with open(os.path.join(app.root_path, 'usuarios/'+request.form['email']+'/datos.json'), 'w') as datos_file:
-            json.dump(datos, datos_file)
+        with open(os.path.join(app.root_path, 'usuarios/'+request.form['email']+'/datos.json'), mode='w', encoding='utf-8') as datos_file:
+            json.dump(datos, datos_file, ensure_ascii=False)
         datos_file.close()
-        with open(os.path.join(app.root_path, 'usuarios/'+request.form['email']+'/historial.json'), 'w') as historial_file:
-            json.dump(historial, historial_file)
+        with open(os.path.join(app.root_path, 'usuarios/'+request.form['email']+'/historial.json'), mode='w', encoding='utf-8') as historial_file:
+            json.dump(historial, historial_file, ensure_ascii=False)
         historial_file.close()
 
         session['usuario'] = request.form['nombre']
@@ -121,20 +122,66 @@ def registro():
 
 @app.route('/carrito', methods=['GET', 'POST'])
 def carrito():
+    #Cálculos generales
     catalogue_data = open(os.path.join(app.root_path,'catalogue/catalogue.json'), encoding="utf-8").read()
     catalogue = json.loads(catalogue_data)
     movies=catalogue['peliculas']
     carr = []
+    suma = 0
     if not('carrito' in session):
         session['carrito'] = {}
-    if 'id' in request.args:
-        id = request.args['id']
-        if session['carrito'][id]==1:
-            session['carrito'].pop(id)
-        else:
-            session['carrito'][id] = session['carrito'][id] - 1
     for id in session['carrito']:
         data = list(filter(lambda film: str(film['id']) == id, movies))[0]
         data['cantidad'] = session['carrito'][id]
         carr.append(data)
-    return render_template('carrito.html', carr=carr)
+        suma += data['cantidad']*data['precio']
+
+    #Renderización inicial
+    if request.method == 'GET':
+        return render_template('carrito.html', carr=carr, suma=round(suma,2))
+
+    #Compra
+    if request.method == 'POST':
+        if 'idAdd' in request.form:
+            id = request.form['idAdd']
+            session['carrito'][id] += 1
+            return redirect(url_for('carrito'))
+        if 'idRemove' in request.form:
+            id = request.form['idRemove']
+            if session['carrito'][id]==1:
+                session['carrito'].pop(id)
+            else:
+                session['carrito'][id] -= 1
+            return redirect(url_for('carrito'))
+        if 'usuario' in session:
+            datos_file = open(os.path.join(app.root_path,'usuarios/'+session['id']+'/datos.json'), encoding="utf-8").read()
+            datos = json.loads(datos_file)
+            if(suma > datos['saldo']):
+                return render_template('carrito.html', carr=carr, suma=round(suma,2), msg='No hay saldo suficiente para realizar la compra.')
+            else:
+                datos['saldo'] -= suma
+                datos['saldo'] = round(datos['saldo'], 2)
+                session['carrito'] = {}
+                with open(os.path.join(app.root_path, 'usuarios/'+session['id']+'/datos.json'), mode='w', encoding='utf-8') as datos_file_output:
+                    json.dump(datos, datos_file_output, ensure_ascii=False)
+                datos_file_output.close()
+                fecha = date.today()
+                historial_file = open(os.path.join(app.root_path,'usuarios/'+session['id']+'/historial.json'), encoding="utf-8").read()
+                historial = json.loads(historial_file)
+                compra = {"carrito":carr, "fecha":fecha.strftime('%d/%m/%Y'), "total":suma}
+                historial['pedidos'].append(compra)
+                with open(os.path.join(app.root_path, 'usuarios/'+session['id']+'/historial.json'), mode='w', encoding='utf-8') as hist_file:
+                    json.dump(historial, hist_file, ensure_ascii=False)
+                hist_file.close()
+
+                return render_template('carrito.html', carr=session['carrito'], suma=0, msg='¡Compra realizada con éxito!')
+        else:
+            return render_template('carrito.html', carr=carr, suma=suma, msg='Necesitas estar logueado para poder comprar')
+
+@app.route('/historial', methods=['GET', 'POST'])
+def historial():
+    if 'usuario' not in session:
+        return redirect(url_for('login'))
+    historial_file = open(os.path.join(app.root_path,'usuarios/'+session['id']+'/historial.json'), encoding="utf-8").read()
+    historial = json.loads(historial_file)
+    return render_template("historial.html", hist=historial['pedidos'])
