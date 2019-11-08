0ac4663668989bd7fc96bf4f5989feebfdf1e793
sergamar <sergamar23@hotmail.es>
2019-11-08 23:02:32 +0100

    getTopVentas hecho

--- P3/getTopVentas.sql
+++ P3/getTopVentas.sql
@@ -1,8 +1,21 @@
 CREATE OR REPLACE FUNCTION getTopVentas(anio INTEGER)
-  RETURNS TABLE(
-    anio INTEGER,
-
+  RETURNS TABLE (
+    agno INTEGER,
+    pelicula VARCHAR,
+    ventas INTEGER
   )
   AS $$
   BEGIN
+    RETURN QUERY
+    SELECT DISTINCT ON (calc.agno) calc.agno, calc.pelicula, calc.ventas FROM(
+      SELECT date_part('year', orders.orderdate)::INTEGER AS agno,
+      imdb_movies.movietitle AS pelicula,
+      SUM(orderdetail.quantity)::INTEGER AS ventas
+      FROM orders INNER JOIN orderdetail ON orderdetail.orderid = orders.orderid
+      INNER JOIN products ON products.prod_id = orderdetail.prod_id
+      INNER JOIN imdb_movies ON imdb_movies.movieid = products.movieid
+      WHERE date_part('year', orders.orderdate) >= anio
+      GROUP BY imdb_movies.movieid, agno
+      ORDER BY agno, ventas DESC, pelicula
+    ) AS calc;
   END; $$ LANGUAGE PLPGSQL;
